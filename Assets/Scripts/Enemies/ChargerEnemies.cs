14cdf2d4f2a265bd48d2f4849a30736efd5ad55d
Anthony Limnios <anthonylimnios@hotmail.com>
2018-11-26 14:55:09 +1100

    Finally fixed library issue and started prototyping enemies

---
+++ Assets/Scripts/Enemies/ChargerEnemies.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LazyTitan.Extensions;
+
+public class ChargerEnemies : EnemyBase
+{
+    public float distance = 100f;
+
+}
---
+++ Assets/Scripts/Enemies/EnemyBase.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LazyTitan.Extensions;
+
+public class EnemyBase : MonoBehaviour
+{
+    public int startingHealth = 5;
+    public int currentHealth;
+
+    bool isHit;
+    bool isDead;
+    bool isInRange;
+
+    protected virtual void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    protected virtual void Update()
+    {
+        if (isInRange)
+        {
+            
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        isHit = true;
+
+        if (isDead)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        gameObject.SetActive(false);
+    }
+}
