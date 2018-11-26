969d3fd2b54149d49a06a76d814d8cabe463b6a7
Scott-Tatham <scottytatham@gmail.com>
2018-11-26 21:49:34 +1100

    Added Attribute to properly draw enum bit flag types.

--- Assets/Scripts/Weapons/Ammunition Components/Ammo.cs
+++ Assets/Scripts/Weapons/Ammunition Components/Ammo.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using LazyTitan.Serialization.Attributes;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,8 @@ public class Ammo : MonoBehaviour
 {
     [SerializeField]
     protected PrimaryFireType primaryFireType;
+    [SerializeField, EnumBitMask]
+    protected EffectorType effectorTypes;
     [SerializeField]
     protected LayerMask impactTypes;
     [SerializeField, Range(0.01f, 5.0f)]
@@ -13,7 +16,11 @@ public class Ammo : MonoBehaviour
 
     protected bool isFired;
     protected float baseDamage, ammoRange, accuracy;
+    protected EffectEvent OnFire, OnEffector, OnEnemyContact, OnEnvironmentContact;
     protected Weapon weapon;
+    protected Effector[] effectors;
+
+    protected delegate void EffectEvent();
 
     public bool GetIsFired() { return isFired; }
     public PrimaryFireType GetPrimaryFireType() { return primaryFireType; }
@@ -24,8 +31,33 @@ public class Ammo : MonoBehaviour
     public void SetAccuracy(float accuracy) { this.accuracy = accuracy; }
     public void SetWeapon(Weapon weapon) { this.weapon = weapon; }
 
+    void Start()
+    {
+        effectors = GetComponents<Effector>();
+    }
+
+    public void FiredCall()
+    {
+        OnFire();
+    }
+
     public void EffectorCall()
     {
+        OnEffector();
+    }
 
+    public void EnemyContactCall()
+    {
+        OnEnemyContact();
+    }
+
+    public void EnvironmentContactCall()
+    {
+        OnEnvironmentContact();
+    }
+
+    public virtual void OnCollisionEnter(Collision col)
+    {
+        // If it has effector types make it's call.
     }
 }
