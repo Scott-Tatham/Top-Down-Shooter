0d31a092c96ec3f2dcf9f6948e3f91f5b9d06e53
Anthony Limnios <anthonylimnios@hotmail.com>
2018-11-29 18:26:24 +1100

    Started working on a cover system for the enemies but will probably change to make it better

--- Assets/Scripts/Enemies/ShootingUnits/BasicShootingEnemy.cs
+++ Assets/Scripts/Enemies/ShootingUnits/BasicShootingEnemy.cs
@@ -9,9 +9,14 @@ public class BasicShootingEnemy : EnemyBase
     public float rotationSpeed;
     public float moveSpeed;
     public float distanceWanted = 20f;
+    public Vector3 targetPosition;
 
     float shotCounter;
     float moveSpeedStore;
+    Collider col;
+
+    
+    LayerMask coverLayer;
 
     protected override void Start()
     {
@@ -29,12 +34,14 @@ public class BasicShootingEnemy : EnemyBase
         Vector3 diff = transform.position - target.position;
         diff.y = 0;
 
-        transform.position = Vector3.MoveTowards(transform.position, target.position + diff.normalized * distanceWanted, moveSpeed * Time.deltaTime);
+        //transform.position = Vector3.MoveTowards(transform.position, target.position + diff.normalized * distanceWanted, moveSpeed * Time.deltaTime);
 
         if (shotCounter >= shotDuration)
         {
             Shoot();
         }
+
+        HandleCover();
     }
 
     void Shoot()
@@ -42,5 +49,42 @@ public class BasicShootingEnemy : EnemyBase
         shotCounter = 0;
 
         Debug.Log("Shooting");
+    }
+
+    public void HandleCover()
+    {
+        col = FindClosestCover();
+
+        if (col == null)
+        {
+            return;
+        }
+
+        Vector3 dirToTarget = target.position - col.transform.position;
+        dirToTarget.Normalize();
+
+        Vector3 targetPos = col.transform.position + (dirToTarget * -1);
+    }
+
+    public Collider FindClosestCover()
+    {
+        Collider[] coverColls = Physics.OverlapSphere(transform.position, 10, coverLayer);
+
+        float mainDist = float.MaxValue;
+
+        Collider closestCol = null;
+
+        for (int i = 0; i < coverColls.Length; i++)
+        {
+            float tDist = Vector3.Distance(coverColls[i].transform.position, transform.position);
+
+            if (tDist < mainDist)
+            {
+                mainDist = tDist;
+                closestCol = coverColls[i];
+            }
+        }
+
+        return closestCol;
     }
 }
